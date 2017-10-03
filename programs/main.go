60c1642183596860df5e4a19a7d66e6ae98a449c
sys_cat <systemcat91@gmail.com>
2017-10-03 18:18:48 +0900

    update web serber

--- programs/main.go
+++ programs/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
+}
 
 func main() {
-	fmt.Println("Hello Docker")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
