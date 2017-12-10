24132e131fe66b8f438af121a7b951483fdc18d5
sys-cat <systemcat91@gmail.com>
2017-12-11 00:29:20 +0900

    add test parameters

--- linelogin_test.go
+++ linelogin_test.go
@@ -1,8 +1,10 @@
 package linelogin
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestNew(t *testing.T) {
@@ -32,4 +34,36 @@ func TestNew(t *testing.T) {
 	} else {
 		t.Fatal("Params.Scope is not string")
 	}
+}
+
+func TestParameters(t *testing.T) {
+	param := New()
+	channel_id := "test channel id"
+	channel_secret := "test channel secret"
+	redirect := "test redirect url"
+	param.Parameters(channel_id, channel_secret, redirect)
+	if param.ChannelID == channel_id {
+		t.Log("channel_id is clean")
+	} else {
+		t.Fatal("channel_id is invalid")
+	}
+	if param.RedirectURL == redirect {
+		t.Log("redirect url is clean")
+	} else {
+		t.Fatal("redirect url is invalid")
+	}
+	if param.State == fmt.Sprint(time.Now().Unix()) {
+		t.Log("state is clean")
+	} else {
+		t.Fatal("state is invalid")
+	}
+	if param.Scope == "profile" {
+		t.Log("scope is clean")
+	} else {
+		t.Fatal("scope is invalid")
+	}
+}
+
+func TestOutputURL(t *testing.T) {
+	t.Log("nothing test")
 }
